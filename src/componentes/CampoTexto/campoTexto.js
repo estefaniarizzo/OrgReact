f63f4ea798d95c73a07180373f3e4f7b3ecd38d2
Estefania Rizzo <tefa49395@gmail.com>
2023-08-03 11:09:46 -0500

    comits hechos

---
+++ src/componentes/CampoTexto/campoTexto.js
@@ -0,0 +1,11 @@
+import './campoTexto.css'
+
+const CampoTexto =(props)=>{
+    console.log("Datos:",props.titulo);
+    const placeholderModificado=`${props.placeholder}...`
+    return <div className='campo-texto'>
+        <label>{props.titulo}</label>
+        <input placeholder={placeholderModificado}/>
+    </div>
+}
+export default CampoTexto
